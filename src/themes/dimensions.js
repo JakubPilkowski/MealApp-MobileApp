e94ce4101165cb44d10ab0d499d80f0c9eccbc2e
Pilkowski <ilsihaterhd@gmail.com>
2020-03-13 20:13:23 +0100

    Domyslny toolbar

---
+++ src/themes/dimensions.js
@@ -0,0 +1,8 @@
+export default {
+    defaultMargin: 12,
+    defaultIconSize:36,
+    defaultPadding:12,
+    defaultBorderRadius:6,
+    defaultBorderWidth:1,
+    toolbarFontSize:24,
+};
---
+++ src/themes/strings.js
@@ -0,0 +1,11 @@
+export default{
+    jadlodajnie:"Jadłodajnie",
+    ulubione: "Ulubione",
+    powiadomienia: "Powiadomienia",
+    kryteria: "Kryteria",
+    logowanie: "Logowanie",
+    rejestracja: "Rejestracja",
+    mapa:"Mapa",
+    mealapp: "MealApp",
+    placeholder:"Coś tu pusto"
+};
