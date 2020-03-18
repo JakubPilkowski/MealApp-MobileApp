752f037d6e0deeb3e4df4955c2c98f0f743d1b49
Pilkowski <ilsihaterhd@gmail.com>
2020-03-18 19:18:14 +0100

    Utworzono mapke, slider dla zestawow oraz upiekszono informacje odnosnie punktow

--- screens/JadlodajnieWiecej.js
+++ screens/JadlodajnieWiecej.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { View, Text, StyleSheet, Animated, ScrollView, Image, TouchableNativeFeedback, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Animated, ScrollView, Image, TouchableNativeFeedback, TouchableOpacity, Dimensions } from 'react-native';
 import Colors from "../src/themes/colors";
 import dimensions from '../src/themes/dimensions';
 import { Divider } from 'react-native-elements';
-// import Strings from "../src/themes/strings";
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import Strings from "../src/themes/strings";
 const HEADER_EXPANDED_HEIGHT = 225;
 const HEADER_COLLAPSED_HEIGHT = 56;
-
+let currentWidth = 0;
 import { Ionicons, FontAwesome, Feather } from '@expo/vector-icons';
+import Danie from '../components/Danie';
+
+const { width, height } = Dimensions.get('window');
 
 export default class JadlodajnieWiecej extends React.Component {
 
@@ -21,6 +25,7 @@ export default class JadlodajnieWiecej extends React.Component {
     }
 
     render() {
+
         const headerHeight = this.state.scrollY.interpolate(
             {
                 inputRange: [0, HEADER_EXPANDED_HEIGHT - HEADER_COLLAPSED_HEIGHT],
@@ -70,7 +75,7 @@ export default class JadlodajnieWiecej extends React.Component {
                         }])}
                     scrollEventThrottle={16}
                 >
-                    <View style={{ flex: 1, flexDirection: 'row', margin: dimensions.defaultMargin }}>
+                    {/* <View style={{ flex: 1, flexDirection: 'row', margin: dimensions.defaultMargin }}>
                         <Image style={styles.image}></Image>
                         <View style={{ flexDirection: 'column', justifyContent: "center", flex: 1 }}>
                             <TouchableOpacity onPress={() => { }} >
@@ -81,59 +86,99 @@ export default class JadlodajnieWiecej extends React.Component {
                                 <Text style={styles.avatarName}>Ocena: </Text>
                             </View>
                         </View>
-                    </View>
-                    <View style={{ flex: 1, flexDirection: 'row' }}>
-                        <View style={{ flexDirection: 'column', justifyContent: "center", alignItems: "center", width: 50 }}>
-                            <Text>cos</Text>
+                    </View> */}
+                    <Text style={{ fontSize: 20, textAlign: "center", marginBottom: dimensions.defaultHugeMargin }}>Zestawy</Text>
+                    <View style={{ flex: 1, flexDirection: 'row', marginBottom: dimensions.defaultMarginBetweenItems }}>
+                        <View style={{ flexDirection: 'column',alignItems:'center', justifyContent: 'flex-end', width: 50 }}>
+                            <TouchableOpacity onPress={() => {
+                                if (currentWidth === 0) {
+                                    return;
+                                }
+                                else {
+                                    currentWidth = currentWidth - (width - 100);
+                                    this.refs.scroll.scrollTo({ x: currentWidth });
+                                }
+                            }}>
+                                <Feather name="arrow-left-circle" color={Colors.primary} size={36}></Feather>
+                            </TouchableOpacity>
                         </View>
-                        <View style={{ flex: 1, flexDirection: 'column' }}>
-                            <Text>18.03.2020</Text>
-                            <Divider style={styles.divider}></Divider>
-                            <View style={{ flexDirection: 'row' }}>
-                                <Text>
-                                    Nazwa
-                                </Text>
-                                <Text>
-                                    Cena
-                                </Text>
+                        <ScrollView horizontal={true}
+                            scrollEnabled={false}
+                            showsHorizontalScrollIndicator={false}
+                            ref={'scroll'}
+                        >
+                            <View style={{ width: width - 100, flexDirection: 'column' }}>
+                                <Text style={{ textAlign: "center", fontSize: dimensions.hugeFontSize }}>17.03.2020</Text>
+                                <Divider style={[styles.divider, { marginBottom: dimensions.defaultSmallMargin, width: "40%", alignSelf: "center" }]}></Divider>
+                                <Danie nazwa="zupka" cena="10"></Danie>
+                                <Danie nazwa="kotlecik" cena="10"></Danie>
                             </View>
-                            <Divider style={styles.divider}></Divider>
-                        </View>
-                        <View style={{ flexDirection: 'column', justifyContent: "center", alignItems: "center", width: 50 }}>
-                            <Text>cos</Text>
+                            <View style={{ width: width - 100, flexDirection: 'column' }}>
+                                <Text style={{ textAlign: "center", fontSize: dimensions.hugeFontSize }}>18.03.2020</Text>
+                                <Divider style={[styles.divider, { marginBottom: dimensions.defaultSmallMargin, width: "40%", alignSelf: "center" }]}></Divider>
+                                <Danie nazwa="zupka2" cena="10"></Danie>
+                                <Danie nazwa="kotlecik2" cena="10"></Danie>
+                            </View>
+                        </ScrollView>
+                        <View style={{ flexDirection: 'column', justifyContent: 'flex-end', alignItems: "center", width: 50 }}>
+                            <TouchableOpacity onPress={() => {
+                                currentWidth = currentWidth + width - 100;
+                                this.refs.scroll.scrollTo({ x: currentWidth });
+                            }}>
+                                <Feather name="arrow-right-circle" color={Colors.primary} size={36}></Feather>
+
+                            </TouchableOpacity>
                         </View>
                     </View>
+                    <Text style={{ textAlign: "center", fontSize: 20, marginBottom: dimensions.defaultHugeMargin }}>Dostępne punkty</Text>
                     <View style={{ marginHorizontal: dimensions.defaultMargin }}>
-                        <Text style={{textAlign:"center"}}>Adresy</Text>
-                        <View style={{ flexDirection: "row", marginVertical: dimensions.defaultSmallMargin, justifyContent:'space-between', alignItems:'center' }}>
-                            <FontAwesome size={36} color={Colors.primary} name="map-marker"></FontAwesome>
-                            <View style={{marginLeft:dimensions.defaultMargin, flexDirection: 'column', flex:1}}>
-                                <Text>ul. kościuszki 19/2</Text>
-                                <Text>ul. Jagiellończyka 34/12</Text>
-                                <Text>ul. Jagiellończyka 34/12</Text>
-                                <Text>ul. Jagiellończyka 34/12</Text>
+                        <Text style={styles.centerAlignText}>Adres</Text>
+                        <View style={{ flexDirection: "row", marginVertical: dimensions.defaultSmallMargin, justifyContent: 'space-between', alignItems: 'center' }}>
+                            <View style={{ width: 50, alignItems: "center" }}>
+                                <FontAwesome size={36} color={Colors.primary} name="map-marker"></FontAwesome>
+                            </View>
+                            <View style={{ marginLeft: dimensions.defaultMargin, flexDirection: 'column', flex: 1 }}>
+                                <Text style={styles.rightAlignText}>Przykładowy adres</Text>
                             </View>
                         </View>
-                        <Text style={{textAlign:"center"}}>Kontakt</Text>
-                        <View style={{ flexDirection: "row", marginVertical: dimensions.defaultSmallMargin, justifyContent: 'center', alignItems:"center" }}>
-                            <FontAwesome size={36} color={Colors.primary} name="phone"></FontAwesome>
-                            <View style={{marginLeft:dimensions.defaultMargin, flexDirection: 'column', flex:1}}>
-                                <Text>ul. kościuszki 19/2</Text>
-                                <Text>ul. Jagiellończyka 34/12</Text>
-                                <Text>ul. Jagiellończyka 34/12</Text>
-                                <Text>ul. Jagiellończyka 34/12</Text>
+                        <Text style={styles.centerAlignText}>Kontakt</Text>
+                        <View style={{ flexDirection: "row", marginVertical: dimensions.defaultSmallMargin, justifyContent: 'center', alignItems: "center" }}>
+                            <View style={{ width: 50, alignItems: "center" }}>
+                                <FontAwesome size={36} color={Colors.primary} name="phone"></FontAwesome>
+                            </View>
+                            <View style={{ marginLeft: dimensions.defaultMargin, flexDirection: 'column', flex: 1 }}>
+                                <Text style={styles.rightAlignText}>Przykładowy kontakt 1</Text>
+                                <Text style={styles.rightAlignText}>Przykładowe kontakt 2</Text>
                             </View>
                         </View>
-                        <Text style={{textAlign:"center"}}>Godziny otwarcia</Text>
-                        <View style={{ flexDirection: "row", marginVertical: dimensions.defaultSmallMargin, justifyContent:"center", alignItems:"center" }}>
-                            <Feather name="clock" size={36} color={Colors.primary}></Feather>
-                            <View style={{marginLeft:dimensions.defaultMargin, flexDirection: 'column', flex:1}}>
-                                <Text>ul. kościuszki 19/2</Text>
-                                <Text>ul. Jagiellończyka 34/12</Text>
-                                <Text>ul. Jagiellończyka 34/12</Text>
-                                <Text>ul. Jagiellończyka 34/12</Text>
+                        <Text style={styles.centerAlignText}>Godziny otwarcia</Text>
+                        <View style={{ flexDirection: "row", marginVertical: dimensions.defaultSmallMargin, justifyContent: "center", alignItems: "center" }}>
+                            <View style={{ width: 50, alignItems: "center" }}>
+                                <Feather name="clock" size={36} color={Colors.primary}></Feather>
+                            </View>
+                            <View style={{ marginLeft: dimensions.defaultMargin, flexDirection: 'column', flex: 1 }}>
+                                <Text style={styles.rightAlignText}>Poniedziałek: 8:00-17:00</Text>
+                                <Text style={styles.rightAlignText}>Przykładowe dane</Text>
+                                <Text style={styles.rightAlignText}>Przykładowe dane</Text>
+                                <Text style={styles.rightAlignText}>Przykładowe dane</Text>
+                                <Text style={styles.rightAlignText}>Przykładowe dane</Text>
+                                <Text style={styles.rightAlignText}>Przykładowe dane</Text>
+                                <Text style={styles.rightAlignText}>Przykładowe dane</Text>
                             </View>
                         </View>
+                    </View>
+                    <Text style={styles.centerAlignText}>Mapa</Text>
+                    <View style={{ borderWidth: 2, borderColor: Colors.accent, margin: dimensions.defaultMargin, }}>
+                        <MapView provider={PROVIDER_GOOGLE} style={{ height: 200, borderColor: Colors.primary, borderWidth: dimensions.borderWidth }} initialRegion={{
+                            latitude: 53.770168,
+                            longitude: 20.470272,
+                            longitudeDelta: 0.003,
+                            latitudeDelta: 0.003
+                        }} minZoomLevel={10} title="adsdasas">
+                            <Marker coordinate={{ latitude: 53.76998768366101, longitude: 20.47289226769843 }} title="Pełny gar" description="ul Walentego Barczewskiego 1 " pinColor={Colors.primary} >
+                                <FontAwesome size={36} color={Colors.primary} name="map-marker"></FontAwesome>
+                            </Marker>
+                        </MapView>
                     </View>
                 </ScrollView>
             </View>
@@ -146,7 +191,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-
     },
     header: {
         backgroundColor: Colors.primary,
@@ -206,4 +250,13 @@ const styles = StyleSheet.create({
         height: dimensions.defaultBorderWidth,
         width: "100%"
     },
+    centerAlignText: {
+        textAlign: 'center',
+        fontSize: dimensions.hugeFontSize,
+    },
+    rightAlignText: {
+        textAlign: 'right',
+        fontSize: dimensions.defaultFontSize,
+        marginRight: dimensions.defaultSmallMargin
+    }
 })
