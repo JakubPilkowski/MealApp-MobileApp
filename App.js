00f709e76f2e52e8e457d79376b2bc67db62aa09
Pilkowski <ilsihaterhd@gmail.com>
2020-04-26 15:30:59 +0200

    Zrobiono logowanie za pomoca googla

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import Dimensions from './src/themes/dimensions';
 import {
   createDrawerNavigator, DrawerItem,
 } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import Jadlodajnie from "./screens/Jadlodajnie";
 import Ulubione from "./screens/Ulubione";
 import Powiadomienia from './screens/Powiadomienia';
@@ -17,157 +19,213 @@ import Logowanie from './screens/Logowanie';
 import EdytujProfil from './screens/EdytujProfil';
 import CustomLoadingComponent from './components/CustomLoadingComponent';
 import WyborLokalizacji from './screens/WyborLokalizacji';
+import Home from './screens/Home';
 
-const Drawer = createDrawerNavigator();
+
+const Stack = createStackNavigator();
 
 export default function App() {
-
   const [isLoading, setIsLoading] = useState(true);
-  const [isStorageLoading, setIsStorageLoading] = useState(true);
-  const [dataSource, setDataSource] = useState([]);
   const [firstUse, setFirstUse] = useState();
   const forFade = ({ current, closing }) => ({
     cardStyle: {
       opacity: current.progress,
     },
   });
-
-  let uzytkownicy = [];
-  let pobranyUzytkownik;
   useEffect(() => {
     fetchStorage();
-  }, firstUse, isLoading, isStorageLoading);
+  }, [isLoading]);
 
   async function fetchStorage() {
-      if (isStorageLoading) {
-        try {
-          setFirstUse(await AsyncStorage.getItem("firstUse"));
-          setIsStorageLoading(false);
-        }
-        catch (error) {
-
-        }
+    if (isLoading) {
+      try {
+        setFirstUse(await AsyncStorage.getItem("firstUse"));
+        setIsLoading(false);
       }
+      catch (error) {
+
+      }
+    }
   }
-  if (isStorageLoading) {
+  if (isLoading) {
     return <CustomLoadingComponent />
   }
   else {
-    if (firstUse !== "true") {
-      return (
-        <WyborLokalizacji onConfirm={(wojewodztwo, miasto) => {
-          (async () => {
-            try {
-              await AsyncStorage.setItem('firstUse', 'true');
-              await AsyncStorage.setItem('wojewodztwo', wojewodztwo);
-              await AsyncStorage.setItem('miasto', miasto);
-              setFirstUse("true");
-            }
-            catch (error) {
-              console.log(error);
-            }
-          })();
-
-        }} />
-      );
-    }
-    else {
-      async function fetchData() {
-        if (isLoading) {
-          setTimeout(async function(){
-          await AsyncStorage.removeItem("firstUse");
-          const res = await Connection.getUserOptions();
-          res
-            .json()
-            .then(res => {
-              res.uzytkownicy.map((uzytkownik) => {
-                uzytkownicy.push(uzytkownik);
-              });
-              uzytkownicy.map((uzytkownik, index) => {
-                if (index === 0) {
-                  pobranyUzytkownik = uzytkownik;
-                }
-              });
-
-              setDataSource(pobranyUzytkownik);
-              setIsLoading(false);
-            })
-            .catch(err => console.log(err));
-      },3000);
-    }
-    }
-      fetchData();
-      if (isLoading) {
-        return (
-          <CustomLoadingComponent />
-        )
-      }
-      else {
-        return (
-          <NavigationContainer>
-            <Drawer.Navigator initialRouteName="Jadlodajnie" drawerContent={(props) => {
-              return (
-                <CustomDrawer {...props} dataSource={dataSource} />
-              )
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={firstUse === "false" ? "Home" : "WyborLokalizacji"} >
+          <Stack.Screen name="WyborLokalizacji" component={WyborLokalizacji}
+            options={{
+              headerStyle: {
+                opacity: 0, height: 0
+              },
+              cardStyleInterpolator: forFade
+            }} />
+          <Stack.Screen name="Home" component={Home}
+            options={{
+              headerStyle: {
+                opacity: 0, height: 0
+              },
+              cardStyleInterpolator: forFade
             }}
-              drawerContentOptions={{
-                activeTintColor: Colors.accent,
-                activeBackgroundColor: Colors.colorTextWhite,
-                inactiveTintColor: Colors.colorTextWhite,
-                inactiveBackgroundColor: 'transparent',
-                labelStyle: { fontSize: 18, fontWeight: 'bold' },
-              }}
-            >
-              <Drawer.Screen name="Jadlodajnie" component={Jadlodajnie}
-                initialParams={{ navigation: Drawer.navigation }}
-                options={
-                  {
-                    drawerIcon: ({ color }) => <MaterialCommunityIcons name="food-fork-drink" size={24} color={color} />,
-                    cardStyleInterpolator: forFade
-                  }
-                } />
-              <Drawer.Screen name="Ulubione" component={Ulubione} options={
-                {
-                  drawerIcon: ({ color }) => <Ionicons name="md-star" size={30} color={color} />,
-                  cardStyleInterpolator: forFade
-                }
-              } />
-              <Drawer.Screen name="Powiadomienia" component={Powiadomienia} options={
-                {
-                  drawerIcon: ({ color }) => <Ionicons name="md-notifications" size={33} color={color} />,
-                  cardStyleInterpolator: forFade
-                }
-              } />
-              <Drawer.Screen name="Mapa" component={Mapa} options={
-                {
-                  drawerIcon: ({ color }) => <Foundation name="map" size={27} color={color} />,
-                  cardStyleInterpolator: forFade
-                }
-              } />
-              <Drawer.Screen name="Logowanie" component={Logowanie}
-                // initialParams={{ navigation: Drawer.navigation }}
-                options={
-                  {
-                    title: "Logowanie",
-                    gestureEnabled: false,
-                    cardStyleInterpolator: forFade
-                  }
-                } />
-              <Drawer.Screen name="EdytujProfil" component={EdytujProfil} options={
-                {
-                  title: "EdytujProfil",
-                  gestureEnabled: false,
-                  cardStyleInterpolator: forFade
-                }
-              } />
-            </Drawer.Navigator>
-          </NavigationContainer>
-
-        );
-      }
-    }
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
   }
+
+
 }
+
+
+
+// export default function App() {
+
+//   const [isLoading, setIsLoading] = useState(true);
+//   // const [isStorageLoading, setIsStorageLoading] = useState(true);
+//   const [dataSource, setDataSource] = useState([]);
+//   const [firstUse, setFirstUse] = useState();
+//   const forFade = ({ current, closing }) => ({
+//     cardStyle: {
+//       opacity: current.progress,
+//     },
+//   });
+//   let uzytkownicy = [];
+//   let pobranyUzytkownik;
+//   useEffect(() => {
+//     fetchStorage();
+//   }, firstUse, isLoading, isStorageLoading);
+
+//   async function fetchStorage() {
+//     if (isStorageLoading) {
+//       try {
+//         setFirstUse(await AsyncStorage.getItem("firstUse"));
+//         setIsStorageLoading(false);
+//       }
+//       catch (error) {
+
+//       }
+//     }
+//   }
+//   if (isStorageLoading) {
+//     return <CustomLoadingComponent />
+//   }
+//   else {
+//     if (firstUse !== "true") {
+//       return (
+//         <WyborLokalizacji onConfirm={(wojewodztwo, miasto) => {
+//           (async () => {
+//             try {
+//               await AsyncStorage.setItem('firstUse', 'true');
+//               await AsyncStorage.setItem('wojewodztwo', wojewodztwo);
+//               await AsyncStorage.setItem('miasto', miasto);
+//               setFirstUse("true");
+//             }
+//             catch (error) {
+//               console.log(error);
+//             }
+//           })();
+
+//         }} />
+//       );
+//     }
+//     else {
+//       async function fetchData() {
+//         if (isLoading) {
+//           setTimeout(async function () {
+//             await AsyncStorage.removeItem("firstUse");
+//             const res = await Connection.getUserOptions();
+//             res
+//               .json()
+//               .then(res => {
+//                 res.uzytkownicy.map((uzytkownik) => {
+//                   uzytkownicy.push(uzytkownik);
+//                 });
+//                 uzytkownicy.map((uzytkownik, index) => {
+//                   if (index === 0) {
+//                     pobranyUzytkownik = uzytkownik;
+//                   }
+//                 });
+
+//                 setDataSource(pobranyUzytkownik);
+//                 setIsLoading(false);
+//               })
+//               .catch(err => console.log(err));
+//           }, 3000);
+//         }
+//       }
+//       fetchData();
+//       if (isLoading) {
+//         return (
+//           <CustomLoadingComponent />
+//         )
+//       }
+//       else {
+//         return (
+//           <NavigationContainer>
+//             <Drawer.Navigator initialRouteName="Jadlodajnie" drawerContent={(props) => {
+//               return (
+//                 <CustomDrawer {...props} dataSource={dataSource} />
+//               )
+//             }}
+//               drawerContentOptions={{
+//                 activeTintColor: Colors.accent,
+//                 activeBackgroundColor: Colors.colorTextWhite,
+//                 inactiveTintColor: Colors.colorTextWhite,
+//                 inactiveBackgroundColor: 'transparent',
+//                 labelStyle: { fontSize: 18, fontWeight: 'bold' },
+//               }}
+//             >
+//               <Drawer.Screen name="Jadlodajnie" component={Jadlodajnie}
+//                 initialParams={{ navigation: Drawer.navigation }}
+//                 options={
+//                   {
+//                     drawerIcon: ({ color }) => <MaterialCommunityIcons name="food-fork-drink" size={24} color={color} />,
+//                     cardStyleInterpolator: forFade
+//                   }
+//                 } />
+//               <Drawer.Screen name="Ulubione" component={Ulubione} options={
+//                 {
+//                   drawerIcon: ({ color }) => <Ionicons name="md-star" size={30} color={color} />,
+//                   cardStyleInterpolator: forFade
+//                 }
+//               } />
+//               <Drawer.Screen name="Powiadomienia" component={Powiadomienia} options={
+//                 {
+//                   drawerIcon: ({ color }) => <Ionicons name="md-notifications" size={33} color={color} />,
+//                   cardStyleInterpolator: forFade
+//                 }
+//               } />
+//               <Drawer.Screen name="Mapa" component={Mapa} options={
+//                 {
+//                   drawerIcon: ({ color }) => <Foundation name="map" size={27} color={color} />,
+//                   cardStyleInterpolator: forFade
+//                 }
+//               } />
+//               <Drawer.Screen name="Logowanie" component={Logowanie}
+//                 // initialParams={{ navigation: Drawer.navigation }}
+//                 options={
+//                   {
+//                     title: "Logowanie",
+//                     gestureEnabled: false,
+//                     cardStyleInterpolator: forFade
+//                   }
+//                 } />
+//               <Drawer.Screen name="EdytujProfil" component={EdytujProfil} options={
+//                 {
+//                   title: "EdytujProfil",
+//                   gestureEnabled: false,
+//                   cardStyleInterpolator: forFade
+//                 }
+//               } />
+//             </Drawer.Navigator>
+//           </NavigationContainer>
+
+//         );
+//       }
+//     }
+//   }
+// }
 
 const styles = StyleSheet.create({
 
---
+++ screens/Home.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, AsyncStorage, View, Text } from 'react-native';
+import Jadlodajnie from "../screens/Jadlodajnie";
+import Ulubione from "../screens/Ulubione";
+import Powiadomienia from '../screens/Powiadomienia';
+import Mapa from '../screens/Mapa';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import CustomDrawer from '../components/CustomDrawer';
+import Connection from '../service/Connection';
+import Colors from '../src/themes/colors';
+import { Ionicons, Foundation, MaterialCommunityIcons } from '@expo/vector-icons';
+import Logowanie from '../screens/Logowanie';
+import EdytujProfil from '../screens/EdytujProfil';
+import CustomLoadingComponent from '../components/CustomLoadingComponent';
+const Drawer = createDrawerNavigator();
+
+const Home = props => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [dataSource, setDataSource] = useState([]);
+    const forFade = ({ current, closing }) => ({
+        cardStyle: {
+            opacity: current.progress,
+        },
+    });
+    let uzytkownicy = [];
+    let pobranyUzytkownik;
+
+    async function fetchData() {
+        if (isLoading) {
+            setTimeout(async function () {
+                await AsyncStorage.removeItem("firstUse");
+                const token = await AsyncStorage.getItem("authToken");
+                console.log(token);
+                if (token !== null) {
+                    const login = await AsyncStorage.getItem("login");
+                    const email = await AsyncStorage.getItem("email");
+                    const avatar = await AsyncStorage.getItem("avatar");
+                    setDataSource({ login: login, email: email, avatar: avatar });
+                    setIsLoading(false);
+                }
+                else {
+                    const res = await Connection.getUserOptions();
+                    res
+                        .json()
+                        .then(res => {
+                            res.uzytkownicy.map((uzytkownik) => {
+                                uzytkownicy.push(uzytkownik);
+                            });
+                            uzytkownicy.map((uzytkownik, index) => {
+                                if (index === 0) {
+                                    pobranyUzytkownik = uzytkownik;
+                                }
+                            });
+
+                            setDataSource(pobranyUzytkownik);
+                            setIsLoading(false);
+                        })
+                        .catch(err => console.log(err));
+                }
+            }, 1000);
+        }
+    }
+    useEffect(() => {
+        fetchData();
+    }, [isLoading]);
+    if (isLoading) {
+        return (
+            <CustomLoadingComponent />
+        )
+    }
+    else {
+        return (
+            <Drawer.Navigator initialRouteName="Jadlodajnie" drawerContent={(props) => {
+                return (
+                    <CustomDrawer {...props} dataSource={dataSource} />
+                )
+            }}
+                drawerContentOptions={{
+                    activeTintColor: Colors.accent,
+                    activeBackgroundColor: Colors.colorTextWhite,
+                    inactiveTintColor: Colors.colorTextWhite,
+                    inactiveBackgroundColor: 'transparent',
+                    labelStyle: { fontSize: 18, fontWeight: 'bold' },
+                }}>
+                <Drawer.Screen name="Jadlodajnie" component={Jadlodajnie}
+                    initialParams={{ navigation: Drawer.navigation }}
+                    options={
+                        {
+                            drawerIcon: ({ color }) => <MaterialCommunityIcons name="food-fork-drink" size={24} color={color} />,
+                            cardStyleInterpolator: forFade
+                        }
+                    } />
+                <Drawer.Screen name="Ulubione" component={Ulubione} options={
+                    {
+                        drawerIcon: ({ color }) => <Ionicons name="md-star" size={30} color={color} />,
+                        cardStyleInterpolator: forFade
+                    }
+                } />
+                <Drawer.Screen name="Powiadomienia" component={Powiadomienia} options={
+                    {
+                        drawerIcon: ({ color }) => <Ionicons name="md-notifications" size={33} color={color} />,
+                        cardStyleInterpolator: forFade
+                    }
+                } />
+                <Drawer.Screen name="Mapa" component={Mapa} options={
+                    {
+                        drawerIcon: ({ color }) => <Foundation name="map" size={27} color={color} />,
+                        cardStyleInterpolator: forFade
+                    }
+                } />
+                <Drawer.Screen name="Logowanie" component={Logowanie}
+                    // initialParams={{ navigation: Drawer.navigation }}
+                    options={
+                        {
+                            title: "Logowanie",
+                            gestureEnabled: false,
+                            cardStyleInterpolator: forFade
+                        }
+                    } />
+                <Drawer.Screen name="EdytujProfil" component={EdytujProfil} options={
+                    {
+                        title: "EdytujProfil",
+                        gestureEnabled: false,
+                        cardStyleInterpolator: forFade
+                    }
+                } />
+            </Drawer.Navigator>
+        );
+    }
+
+}
+export default Home;
+
