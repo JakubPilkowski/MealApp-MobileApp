d96075eff3c44aeebffeda3f4cf94e03577b0b1a
Pilkowski <ilsihaterhd@gmail.com>
2020-05-24 17:48:25 +0200

    Wstepne pobieranie jadlodajni z api

--- components/Content.js
+++ components/Content.js
@@ -5,13 +5,12 @@ import Colors from '../src/themes/colors';
 import Dimensions from '../src/themes/dimensions';
 
 
-const Danie = props => {
+const Content = props => {
+    const type = props.type;
+
     return (
         <View key={props.id} style={styles.container}>
-            <View style={styles.information}>
-                <Text style={styles.danie}>{props.nazwa}</Text>
-                <Text style={styles.cena}>{props.cena} zł</Text>
-            </View>
+                <Text style={styles.danie}>{props.content}</Text>
             <Divider style={styles.divider}></Divider>
         </View>
     );
@@ -22,21 +21,9 @@ const styles = StyleSheet.create({
     container: {
         marginTop: Dimensions.defaultMargin
     },
-    information: {
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between"
-    },
     danie: {
-        flex: 1,
         fontSize: Dimensions.defaultFontSize,
-        textAlign: "left"
-    },
-    cena: {
-        // flex: 1,
-        fontSize: Dimensions.defaultFontSize,
-        // textAlign: "right",
-        textAlignVertical:'center'
+        textAlign: "center"
     },
     divider: {
         backgroundColor: Colors.accent,
@@ -45,4 +32,4 @@ const styles = StyleSheet.create({
 });
 
 
-export default Danie;
+export default Content;
--- components/Zestaw.js
+++ components/Zestaw.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, Dimensions, StyleSheet } from 'react-native';
 import { Divider } from 'react-native-elements'
-import Danie from './Danie';
+// import Danie from './Danie';
 import dimensions from '../src/themes/dimensions';
 import Colors from '../src/themes/colors';
 
@@ -14,8 +14,8 @@ const Zestaw = (props)=> {
             <View style={{ width: width - 100, flexDirection: 'column' }}>
                 <Text style={{ textAlign: "center", fontSize: dimensions.hugeFontSize }}>{props.date}</Text>
                 <Divider style={[styles.divider, { marginBottom: dimensions.defaultSmallMargin, width: "40%", alignSelf: "center" }]}></Divider>
-                <Danie nazwa={props.name} cena={props.price}></Danie>
-                <Danie nazwa={props.name} cena={props.price}></Danie>
+                {/* <Danie nazwa={props.name} cena={props.price}></Danie>
+                <Danie nazwa={props.name} cena={props.price}></Danie> */}
             </View>
         )
 
