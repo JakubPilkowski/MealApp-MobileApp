d2877461cc3bd2c2bdc2e62093b1942570b6b40b
Pilkowski <ilsihaterhd@gmail.com>
2020-06-03 13:56:10 +0200

    Zaimplementowano api do jadlodajnii wiecej, ukonczono wiekszosc widokow, ustawiono placeholdery na wypadek pustych informacji

--- components/JadlodajnieLocalizationDetails.js
+++ components/JadlodajnieLocalizationDetails.js
@@ -1,31 +1,57 @@
 import React from 'react';
-import { View, Text, StyleSheet} from "react-native";
-
-
+import { View, Text, StyleSheet } from "react-native";
 import JadlodajniaNoteWithIcon from './JadlodajniaNoteWithIcon';
 import dimensions from '../src/themes/dimensions';
 import Colors from '../src/themes/colors';
-
 import { FontAwesome, Feather } from 'react-native-vector-icons';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-
-
 
 const JadlodajnieLocalizationDetails = props => {
 
     const informacje = props.informacje;
+
+    let adres;
+    let kontakt;
+    let godzinyOtwarcia;
+
+    if (informacje.address !== "") {
+        adres = informacje.address;
+    }
+    else {
+        adres = "Ta jadłodajnia nie udostępnia adresu";
+    }
+
+
+    if (informacje.phoneNumber !== "") {
+        kontakt = informacje.phoneNumber;
+    }
+    else {
+        kontakt = "Ta jadłodajnia nie udostępnia kontaktu";
+    }
+
+
+
+    if (informacje.openHoursList.length > 0) {
+        godzinyOtwarcia = informacje.openHoursList.map((openHour) => renderGodzinyOtwarcia(openHour));
+    }
+    else {
+        godzinyOtwarcia =
+            <Text style={styles.rightAlignText}>Ta Jadłodajnia nie udostępnia godzin otwarcia</Text>
+    }
+
     return (
         <View style={{ flex: 1 }}>
-            <View style={{ marginHorizontal: dimensions.defaultMargin }}>
-                 <Text style={styles.centerAlignText}>Adres</Text>
+            <View style={styles.infoBackground}>
+                <Text style={styles.centerAlignText}>Adres</Text>
                 <JadlodajniaNoteWithIcon icon={
                     <FontAwesome size={36} color={Colors.primary} name="map-marker"></FontAwesome>
                 } content={
                     <View style={styles.textContainer}>
-                        <Text style={styles.rightAlignText}>{informacje.address}</Text>
+                        <Text style={styles.rightAlignText}>{adres}</Text>
                     </View>
                 } />
-                
+            </View>
+            <View style={styles.infoBackground}>
                 <Text style={styles.centerAlignText}>Kontakt</Text>
                 <JadlodajniaNoteWithIcon
                     icon={
@@ -34,77 +60,82 @@ const JadlodajnieLocalizationDetails = props => {
                     content=
                     {
                         <View style={styles.textContainer}>
-                            <Text style={styles.rightAlignText}>{informacje.phoneNumber}</Text>
+                            <Text style={styles.rightAlignText}>{kontakt}</Text>
                         </View>
                     }
                 />
+            </View>
+            <View style={styles.infoBackground}>
+
                 <Text style={styles.centerAlignText}>Godziny otwarcia</Text>
-                {/*
                 <JadlodajniaNoteWithIcon
                     icon={
                         <Feather name="clock" size={36} color={Colors.primary}></Feather>
                     }
                     content={
                         <View style={styles.textContainer}>
-                            {informacje.godzinyOtwarcia.map(godzina => renderGodzinyOtwarcia(godzina))}
+                            {godzinyOtwarcia}
                         </View>
                     }
-                /> */}
-            {/* nie ma open hours wypełnionych */}
-            
+                />
             </View>
-            <Text style={styles.centerAlignText}>Mapa</Text>
-            <View style={{ borderWidth: 2, borderColor: Colors.accent, margin: dimensions.defaultMargin, }}>
-                <MapView provider={PROVIDER_GOOGLE}
-                scrollEnabled={false}
-                rotateEnabled={false}
-                pitchEnabled={false}
-                zoomEnabled={false}
-                style={{ height: 200, borderColor: Colors.primary, borderWidth: dimensions.borderWidth }} initialRegion={{
-                    latitude: informacje.latitude,
-                    longitude: informacje.longitude,
-                    longitudeDelta: 0.003,
-                    latitudeDelta: 0.003
-                }} minZoomLevel={10} >
-                    <Marker coordinate={{
-                        latitude: informacje.latitude,
-                        longitude: informacje.longitude
-                    }}>
-                        <FontAwesome size={36} color={Colors.primary} name="map-marker"></FontAwesome>
-                    </Marker>
-                </MapView>
+            <View style={styles.infoBackground}>
+
+                <Text style={[styles.centerAlignText, { marginBottom: 6 }]}>Mapa</Text>
+                <View >
+                    <MapView provider={PROVIDER_GOOGLE}
+                        scrollEnabled={false}
+                        rotateEnabled={false}
+                        pitchEnabled={false}
+                        zoomEnabled={false}
+                        style={{ height: 200, borderColor: Colors.primary, borderWidth: dimensions.borderWidth }} initialRegion={{
+                            latitude: informacje.latitude,
+                            longitude: informacje.longitude,
+                            longitudeDelta: 0.003,
+                            latitudeDelta: 0.003
+                        }} minZoomLevel={10} >
+                        <Marker coordinate={{
+                            latitude: informacje.latitude,
+                            longitude: informacje.longitude
+                        }}>
+                            <FontAwesome size={36} color={Colors.primary} name="map-marker"></FontAwesome>
+                        </Marker>
+                    </MapView>
+                </View>
             </View>
         </View>
     );
 }
 
-function renderKontakty(kontakt) {
+function renderGodzinyOtwarcia(openHour) {
+    const result = openHour.day + ": " + openHour.fromHour + "-" + openHour.toHour;
     return (
-        <Text style={styles.rightAlignText}>{kontakt.numer}</Text>
-    );
-}
-
-function renderGodzinyOtwarcia(godzina) {
-    return (
-        <Text style={styles.rightAlignText}>{godzina.godzina}</Text>
+        <Text style={styles.rightAlignText}>{result}</Text>
     );
 }
 
 const styles = StyleSheet.create({
 
+    infoBackground: {
+        backgroundColor: Colors.colorTextWhite,
+        marginHorizontal: 12,
+        borderRadius: 12,
+        padding: 6,
+        marginBottom: 24
+    },
     textContainer: {
         marginLeft: dimensions.defaultMargin,
         flexDirection: 'column',
-        flex: 1
+        flex: 1,
     },
     rightAlignText: {
         textAlign: 'left',
-        fontSize: dimensions.defaultFontSize,
+        fontSize: 16,
         marginRight: dimensions.defaultSmallMargin
     },
     centerAlignText: {
         textAlign: 'center',
-        fontSize: dimensions.hugeFontSize,
+        fontSize: 20,
     },
 
 });
--- components/ZestawsView.js
+++ components/ZestawsView.js
@@ -1,39 +1,32 @@
 import React from 'react';
 import { View, Text, Dimensions, StyleSheet } from 'react-native';
 import { Divider } from 'react-native-elements'
-// import Danie from './Danie';
 import dimensions from '../src/themes/dimensions';
 import Colors from '../src/themes/colors';
 import Zestaw from './Zestaw';
 
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 
-
-
-const ZestawsView = (props)=> {
+const ZestawsView = (props) => {
     const date = props.date.split('T');
-
-
-        return (
-            <View style={{ width: width - 100, flexDirection: 'column' }}>
-                <Text style={{ textAlign: "center", fontSize: dimensions.hugeFontSize }}>{date[0]}</Text>
-                <Divider style={[styles.divider, { marginBottom: dimensions.defaultSmallMargin, width: "40%", alignSelf: "center" }]}></Divider>
-                {props.contentList.map(zestaw => renderZestaw(zestaw))}
-            </View>
-        )
-
+    return (
+        <View style={{ width: width - 120, flexDirection: 'column' }}>
+            <Text style={{ textAlign: "center", fontSize: dimensions.hugeFontSize }}>{date[0]}</Text>
+            <Divider style={[styles.divider, { marginBottom: dimensions.defaultMargin, width: "40%", alignSelf: "center" }]}></Divider>
+            {props.contentList.map(zestaw => renderZestaw(zestaw))}
+        </View>
+    )
 }
 
-function renderZestaw(zestaw){
-    if (zestaw.type === "DAILY"){
+function renderZestaw(zestaw) {
+    if (zestaw.type === "DAILY") {
         return (
             <Zestaw id={zestaw.id} content={zestaw.content} />
         )
     }
 }
 
-
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
     divider: {
         backgroundColor: Colors.accent,
         height: dimensions.defaultBorderWidth,
